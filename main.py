314bdda4b88f026a07e2369d8a1c99f1e1d629c9
Katherine Sullivan <katms729@gmail.com>
2015-03-04 22:06:14 -0500

    enqueue and correct_input

--- main.py
+++ main.py
@@ -45,36 +45,113 @@ check sample output
 ERROR CHECKING (non-existent devices, terminate nonexistent process)
 """
 
-DEVICE_PREFIXES = "cdp"
-SNAPSHOT_OPTIONS = "r"+DEVICE_PREFIXES
-COMMANDS = "ASt"
+def correct_input(prompt, is_correct):
+    inp = input(prompt)
+    if is_correct(inp):
+        return inp
+    else:
+        return correct_input(prompt, is_correct)
 
-def get_device(device):
-    pass
+class Device_Queue():
+    def __init__(self, name):
+        self.name = name
+        self.queue = []
 
-def snapshot(option=None):
-    pass
+    def dequeue(self):
+        if self.queue:
+            return self.queue.pop(0)
+        else:
+            return None
 
-def prompt():
-    signal = input("$ ")
+    def enqueue(self, process):
+        filename = input("Filename: ")
+        memstart = correct_input("Memstart: ",lambda i: i.isdigit())
+        if "p" in self.name:
+            rw = "w"
+        else:
+            rw = correct_input("R/W: ", lambda i: i.lower in "rw")
+        length = correct_input("File length: ", lambda i: i.isdigit())
 
-    if len(signal) == 1 and signal in COMMANDS:
-        return signal
+        self.queue.append((process, filename, memstart, rw, length))
 
-    elif len(signal) == 2 and (signal[0] == 'S' and signal[1] in SNAPSHOT_OPTIONS) \
-                            or (signal[0].lower() in "cdp" and signal[1].isdigit()):
-        return signal
+
+DEVICE_PREFIXES = "pdc"
+def is_command(command):
+    SNAPSHOT_OPTIONS = "r"+DEVICE_PREFIXES
+    COMMANDS = "At"
+
+    #signal = input("$ ")
+    # todo: why is this only printing every other string? I think it's skipping some, too
+    #print(signal)
+
+    if len(command) == 1 and command in COMMANDS:
+        return True
+
+    elif len(command) == 2 and ((command[0] == 'S' and command[1] in SNAPSHOT_OPTIONS)
+                           or (command[0].lower() in DEVICE_PREFIXES and command[1].isdigit())):
+        return True
 
     else:
-        return prompt()
+        return False
 
+
+ready_queue = []
+device_queues = {"p": None, "d": None, "c": None, "r": ready_queue}
+pid=1
+
+
+def get_device(device_name):
+    pass
+
+def snapshot(option):
+    """if option == 'r':
+        header = "PID"
+        print(header)
+        for process in ready_queue:
+            print(process)
+    # since I don't have the means to add them yet...
+    else:
+        header = "PID \t Filename \t Memstart \t R/W \t File length"
+        print(header)
+        devices = device_queues[option]
+        for device in devices:
+            if devices[device]:
+                for process in devices[device]:
+                    for info in process:
+                        print(info)"""
+    for t in device_queues:
+        print(t, device_queues[t])
+
+# sys gen
+
+def add_devices(prefix, count):
+    devices = {}
+    for n in range(1, count+1):
+        name = prefix+str(n)
+        devices[name] = []
+    device_queues[prefix] = devices
+
+def get_int(message):
+    return int(correct_input(message, lambda n: n.isdigit()))
+
+printers = get_int("Printers: ")
+disks = get_int("Disks: ")
+cds = get_int("CDs: ")
+
+for name, number in zip(DEVICE_PREFIXES, (printers, disks, cds)):
+    add_devices(name, number)
+
+
+#running
 while True:
-    signal = prompt()
+    signal = correct_input("$ ", is_command)
     if len(signal) == 2:
         if signal[0] == "S":
             print("Snapshot", signal[1])
+            snapshot(signal[1])
 
         else:
+            device = get_device(signal)
             if signal[0].isupper():
                 print(signal, "completed")
             else:
@@ -82,8 +159,13 @@ while True:
 
     else:
         if signal == 't':
-            print("Terminating")
+            if ready_queue:
+                ready_queue.pop(0)
+            else:
+                print("No processes running.")
         elif signal == 'A':
             print("New process")
-        elif signal == 'S':
-            print("Snapshot()")
+            ready_queue.append(pid)
+            pid+=1
+        """elif signal == 'S':
+            print("Snapshot()")"""
