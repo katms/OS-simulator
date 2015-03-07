dcba6ac57e9514cb7734113d117ac4002578cf89
Katherine Sullivan <katms729@gmail.com>
2015-03-07 00:40:42 -0500

    Done but some modifications to do jic

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
 """
-running:
-handle system calls and interrupts, indicated by keyboard input
-
-check upper/lowercase
-
-[d]#
-request the named device
-prompt for parameters:
-filename,
-starting memory address (int),
-"r" or "w" (if printer must be w),
-if "w" how long the file is
-enqueue this process's PCB to the device's queue
-
-[D]#
-named device signals completion of its current process, move it to the back of the ready queue
-
-
-S
-Snapshot
-should be < 24 lines (one line per process?)
-
-wait for option: r - ready, p - printers, d - disks, c - CD/RW queues
-
-check sample output
-
-
-
-ERROR CHECKING (non-existent devices, terminate nonexistent process)
+Katherine Sullivan
+OS hw #1
 """
 
-def verify_input(prompt, is_correct, print_error=None):
-    # repeat until the user provides acceptable input
-    inp = input(prompt)
-    if is_correct(inp):
-        return inp
-    else:
-        # optional error message, can use input
-        if print_error:
-            print(print_error(inp))
-        return verify_input(prompt, is_correct, print_error)
-
 class Device_Queue():
+    """Manages the process queue for a single device."""
     def __init__(self, name):
+        # so far the name's only real purpose is in determining if this is a printer
         self.name = name
         self.queue = []
 
@@ -53,11 +17,14 @@ class Device_Queue():
             return None
 
     def enqueue(self, process):
+        # prompt for additional arguments
         filename = input("Filename: ")
         memstart = verify_input("Starting memory address: ",lambda i: i.isdigit())
+        # can only write to a printer
         if "p" in self.name:
             rw = "w"
         else:
+            # make sure r/w is lowercase
             rw = verify_input("R/W: ", lambda i: i.lower() in "rw").lower()
         length = verify_input("File length: ", lambda i: i.isdigit())
 
@@ -67,33 +34,43 @@ class Device_Queue():
         return bool(self.queue)
 
     def __str__(self):
+        # empty device queues don't print
         if not self.queue:
             return ""
-        # todo: proper spacing, move header and count from there
-        # todo: it pritns differently in Pycharm and out of it so maybe do something more complicated
-        # todo: or switch from columns
+        # print every process in order in columns
+
+        # I'm just going to assume this is aligned with the header
+        # because ensuring it would be really complicated
+        # I tried
+        # and then I noticed the same code aligns differently depending where I run it
+        # and then I gave up
+        # this combination of tabs works on at least three mediums, including eniac
         res = '-'+self.name+'-\n'
         for process in self.queue:
             line = "\t"+process[0]+"\t"+process[1]+"\t\t"+process[2]+"\t\t"+process[3]+"\t"+process[4]+'\n'
-            #print(line)
             res+=line
-
-            line = "\t".join(process)
-            #res+='\t'+line+'\n'
         return res
 
-    def test_str(self):
-        line = ""
 
-
-DEVICE_PREFIXES = "pdc"
-
+# put in a class primarily because using globals was bothering me
 class Device_Manager():
+    """
+    Manages all queues (including CPU queue[s]) by device type
+    """
+    DEVICE_PREFIXES = "pdc"
     def __init__(self, printers, disks, cds):
         self.printers = {}
         self.disks = {}
         self.cds = {}
 
+        # the front is considered the process in the CPU
+        self.ready_queue = []
+
+        # incrementing process ID
+        self.pid=1
+
+        # device queues are mapped to integers to ensure they print in numerical order
+        # that was important right
         for n in range(1, printers+1):
             self.printers[n] = Device_Queue('p'+str(n))
 
@@ -103,11 +80,29 @@ class Device_Manager():
         for n in range(1, cds+1):
             self.cds[n] = Device_Queue('c'+str(n))
 
+    def new_process(self):
+        # store pid as a string because so far
+        # there's no reason I should have to do this later on whenever I use it
+        self.ready_queue.append(str(self.pid))
+        self.pid+=1
+
+    def terminate(self):
+        # terminate current process
+        if self.ready_queue:
+            self.ready_queue.pop(0)
+        else:
+            print("No processes running.")
+
     def get(self, name):
-        # prefix is in "pdc"
+        # returns the named device or None
+        # guaranteed: prefix is in "pdc"
         name = name.lower()
         prefix = name[0]
         index = int(name[1:])
+
+        # so my IDE will stop warning me about this
+        device_type = {} # {}.get(k) -> None
+
         if prefix == 'p':
             device_type = self.printers
         elif prefix == 'd':
@@ -117,7 +112,9 @@ class Device_Manager():
         return device_type.get(index)
 
     def get_all(self, prefix):
-        # get all devices of one type
+        # guaranteed: prefix is in "pdc"
+        # get all device queues of one type
+        devices = ()
         if prefix == 'p':
             devices = self.printers
         elif prefix == 'd':
@@ -126,9 +123,49 @@ class Device_Manager():
             devices = self.cds
         return devices.values()
 
+    def snapshot(self, option):
+        """output all processes of some given set of queues"""
+        output=""
+        if option == 'r':
+            header = "\tPID"
+            for process in self.ready_queue:
+                output+=('\t'+process+'\n')
+        else:
 
+            for device_queue in self.get_all(option):
+                if device_queue:
+                    output+=str(device_queue)
+            header = "\t".join(("\tPID", "Filename", "Memstart", "R/W", "File length"))
 
+        line_count=0
+        MAX_LINES = 23
+        # only print MAX_LINES lines at a time, and reiterate the header
+        for line in output.split("\n"):
+            if line: # skip the blank final/only line that keeps printing the header and itself
+                if line_count == 0:
+                    print(header)
+                    line_count+=1
+                print(line)
+                line_count+=1
+                if line_count >= MAX_LINES:
+                    input("Press enter for more")
+                    line_count=0
 
+DEVICE_PREFIXES = Device_Manager.DEVICE_PREFIXES
+
+# keep asking until the user provides acceptable input
+def verify_input(prompt, is_correct, print_error=None):
+    inp = input(prompt)
+    if is_correct(inp):
+        return inp
+    else:
+        # optional error message, can use input
+        if print_error:
+            print(print_error(inp))
+        # ask again and return that result
+        return verify_input(prompt, is_correct, print_error)
+
+# one of the lengthier input verifiers, recognizes commands
 def is_command(command):
     SNAPSHOT_OPTIONS = "r"+DEVICE_PREFIXES
     COMMANDS = "At"
@@ -138,88 +175,66 @@ def is_command(command):
     elif len(command) == 2 and (command[0] == 'S' and command[1] in SNAPSHOT_OPTIONS):
         # snapshot
         return True
-    elif command[0].lower() in DEVICE_PREFIXES and command[1:].isdigit():
+    elif len(command) >=2 and command[0].lower() in DEVICE_PREFIXES and command[1:].isdigit():
         # device name
         return True
     else:
         return False
 
 
-ready_queue = []
-pid=1
+def main():
+
+    # sys_gen
+    def get_int(message):
+        # handles getting and parsing an integer
+        return int(verify_input(message, lambda n: n.isdigit()))
+
+    manager = Device_Manager(get_int("Printers: "), get_int("Disks: "), get_int("CDs: "))
 
 
-def snapshot(option):
-    # todo: snapshot
-    if option == 'r':
-        print("PID")
-        for process in ready_queue:
-            print(process)
-    else:
-        header = []
-        LENGTH = 10
-        """for label in ("    PID", "Filename", "Memstart", "R/W", "File length"):
-            spaces = ' '*(LENGTH-len(label))
-            header.append(label+spaces)"""
-        header = "\t".join(("\tPID", "Filename", "Memstart", "R/W", "File length"))
-            #("\tPID", "Filename", "Memstart", "R/W", "File length")) #"PID \t Filename \t Memstart \t R/W \t File length"
-        print(header)
-
-        #print(manager.get_all(option))
-
-        for device_queue in manager.get_all(option):
-            #print(type(device_queue))
-            # todo: 24 lines
-            if device_queue:
-                print(device_queue)
-
-        """for device in filter(lambda k: option in k, devices):
-            print(device)
-            for process_info in devices[device].queue:
-                print(process_info)"""
-
-
-# sys gen
-def get_int(message):
-    return int(verify_input(message, lambda n: n.isdigit()))
-
-manager = Device_Manager(get_int("Printers: "), get_int("Disks: "), get_int("CDs: "))
-
-#running
-while True:
-    signal = verify_input("$ ", is_command, lambda c: "'"+c+"' is not a recognized command.")
-    if len(signal) >= 2: # in case there are >=10 devices of a single type
-        if signal[0] == "S":
-            snapshot(signal[1])
-
+    #running
+    def unrecognized(command):
+        # for printing an error message
+        # skip this for blank input
+        if command and not command.isspace():
+            return "'"+command+"' is not a recognized command."
         else:
-            # device interrupt
-            device = manager.get(signal)
-            if device is None:
-                print("No such device.")
-            elif signal[0].isupper():
-                # device completion
-                if device:
-                    pcb = device.deque()[0]
-                    ready_queue.append(pcb)
-                else:
-                    print("Device queue is empty.")
-            else:
-                # device request
-                if ready_queue:
-                    device.enqueue(ready_queue.pop(0))
-                else:
-                    print("No process in CPU.")
+            return ""
 
-    else:
-        if signal == 't':
-            # terminate current CPU process
-            if ready_queue:
-                ready_queue.pop(0)
+    while True:
+        signal = verify_input("$ ", is_command, unrecognized)
+        # if there are >=10 devices of a single type, the name could be at least 3 characters
+        if len(signal) >= 2:
+            # is_command only accepts two-char strings beginning S so it must be the right length
+            if signal[0] == "S":
+                manager.snapshot(signal[1])
+
             else:
-                print("No processes running.")
-        elif signal == 'A':
-            # queue new process
-            # store pid as a string because so far there's no reason I should have to do this later
-            ready_queue.append(str(pid))
-            pid+=1
+                # device interrupt
+                device = manager.get(signal)
+                if device is None:
+                    print("No such device.")
+                elif signal[0].isupper():
+                    # device completion
+                    if device:
+                        pcb = device.deque()[0]
+                        manager.ready_queue.append(pcb)
+                    else:
+                        print("Device queue is empty.")
+                else:
+                    # device request
+                    if manager.ready_queue:
+                        device.enqueue(manager.ready_queue.pop(0))
+                    else:
+                        print("No process in CPU.")
+
+        else: # signal in "At"
+            if signal == 't':
+                # terminate current CPU process
+                manager.terminate()
+            elif signal == 'A':
+                # queue new process
+                manager.new_process()
+
+if __name__ == '__main__':
+    main()
